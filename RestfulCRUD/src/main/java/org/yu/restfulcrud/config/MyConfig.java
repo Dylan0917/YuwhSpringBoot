257ff544047d9191c356eb46a91418929abfa898
Dylan0917_Mac <46637586+Dylan0917@users.noreply.github.com>
2021-01-15 08:28:23 +0800

    静态资源添加

---
+++ RestfulCRUD/src/main/java/org/yu/restfulcrud/config/MyConfig.java
@@ -0,0 +1,11 @@
+package org.yu.restfulcrud.config;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName MyConfig.java
+ * @Description TODO
+ * @createTime 2021年01月14日 22:28:00
+ */
+public class MyConfig {
+}
