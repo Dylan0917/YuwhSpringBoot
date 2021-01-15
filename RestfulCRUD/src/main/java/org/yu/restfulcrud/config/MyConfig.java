a5bc39a958900fdd9f806f8267e4f3398d5d3251
Dylan0917_Mac <46637586+Dylan0917@users.noreply.github.com>
2021-01-15 08:30:18 +0800

    修改

--- RestfulCRUD/src/main/java/org/yu/restfulcrud/config/MyConfig.java
+++ RestfulCRUD/src/main/java/org/yu/restfulcrud/config/MyConfig.java
@@ -1,5 +1,10 @@
 package org.yu.restfulcrud.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * @author admin
  * @version 1.0.0
@@ -7,5 +12,14 @@ package org.yu.restfulcrud.config;
  * @Description TODO
  * @createTime 2021年01月14日 22:28:00
  */
-public class MyConfig {
+@Configuration
+//@EnableWebMvc 该注解会放弃Springboot的所有默认配置
+public class MyConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login.html").setViewName("login");
+//        WebMvcConfigurer.super.addViewControllers(registry);
+    }
 }
