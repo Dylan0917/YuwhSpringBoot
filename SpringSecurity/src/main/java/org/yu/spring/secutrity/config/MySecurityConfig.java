4ad436e0a5c54e5a40b1e3d78fbf7a3781829332
Dylan0917_Mac <46637586+Dylan0917@users.noreply.github.com>
2021-02-10 13:16:47 +0800

    安全部分完结

--- SpringSecurity/src/main/java/org/yu/spring/secutrity/config/MySecurityConfig.java
+++ SpringSecurity/src/main/java/org/yu/spring/secutrity/config/MySecurityConfig.java
@@ -1,8 +1,10 @@
 package org.yu.spring.secutrity.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 /**
  * @author yu.wenhua
@@ -15,10 +17,29 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //定制授权规则
-        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/level1/**");
+        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/level1/**")
+                .hasRole("VIP1").antMatchers("/level2/**").hasRole("VIP2")
+        .antMatchers("/level3").hasRole("VIP3");
 
+        //开启自动配置的登陆功能
+        http.formLogin()
+                .usernameParameter("user")
+                .passwordParameter("pwd")
+                .loginPage("/userlogin");
+        //开启自动配置的注销功能
+//        <form th:action="@{/logout}" method="post">
+//		<input type="submit" value="注销">
+//	</form>
+        http.logout()
+                .logoutSuccessUrl("/");//注销成功后返回url
+        http.rememberMe(); //开启自动配置的记住我cookie
+    }
 
-
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+        auth.inMemoryAuthentication().passwordEncoder(new SCryptPasswordEncoder()).withUser("admin").password(new SCryptPasswordEncoder().encode("123456")).roles("VIP1","VIP2")
+                .and().passwordEncoder(new SCryptPasswordEncoder()).withUser("user").password(new SCryptPasswordEncoder().encode("123456")).roles("VIP2","VIP3")
+                .and().passwordEncoder(new SCryptPasswordEncoder()).withUser("wangwu").password(new SCryptPasswordEncoder().encode("123456")).roles("VIP1","VIP3");
     }
 }
